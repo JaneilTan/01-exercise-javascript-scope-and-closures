981cbcccc8281426527b5693cdeec6b9f2606529
Janeil Tan <janeil.tan@developersinstitute.co.nz>
2023-03-09 18:40:29 +1300

    Create a program that remembers a name

--- Submission/script.js
+++ Submission/script.js
@@ -1,4 +1,15 @@
 // Write a getName function
+/*Input: John Lennon
+Output: John Lennon
+*/
+let rememberedName;
+const getName = (name) => {
+    if (rememberedName === undefined) {
+        rememberedName = name;
+    }
+    return rememberedName;
+    
+};
 
 /**
  * NOTE! Do not delete the JavaScript below
